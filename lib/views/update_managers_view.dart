dfe06fab53cc2eb21d49cfd08d51667d00da14a4
larissabeatrizcostaa <larissabeatrizcostaa@gmail.com>
2024-07-15 00:26:38 +0000

    internacionalização página updateManager

--- lib/views/update_managers_view.dart
+++ lib/views/update_managers_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 import '../models/manager_model.dart';
@@ -35,9 +36,9 @@ class UpdateManagers extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                'Editar Gerentes',
-                style: TextStyle(
+              Text(
+                AppLocalizations.of(context)!.updateManagerTitle,
+                style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
@@ -45,10 +46,12 @@ class UpdateManagers extends StatelessWidget {
               const SizedBox(height: 20),
               TextFormField(
                 controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Nome'),
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.nameLabel,
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Por favor, informe um nome';
+                    return AppLocalizations.of(context)!.insertName;
                   }
                   return null;
                 },
@@ -56,8 +59,8 @@ class UpdateManagers extends StatelessWidget {
               const SizedBox(height: 20),
               TextFormField(
                 controller: _phoneController,
-                decoration: const InputDecoration(
-                  labelText: 'Telefone',
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.phoneLabel,
                   hintText: '(xx) xxxxx-xxxx',
                 ),
                 inputFormatters: [
@@ -65,9 +68,9 @@ class UpdateManagers extends StatelessWidget {
                 ],
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Insira um telefone';
+                    return AppLocalizations.of(context)!.insertPhone;
                   } else if (value.length < 10) {
-                    return 'O telefone precisa ter 10 digitos';
+                    return AppLocalizations.of(context)!.phoneDigits;
                   }
                   return null;
                 },
@@ -75,15 +78,15 @@ class UpdateManagers extends StatelessWidget {
               const SizedBox(height: 20),
               TextFormField(
                 controller: _stateController,
-                decoration: const InputDecoration(
-                  labelText: 'Estado: ',
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.stateLabel,
                   hintText: 'UF',
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Insira um estado';
+                    return AppLocalizations.of(context)!.insertState;
                   } else if (value.length > 2) {
-                    return 'Digite o UF de seu estado';
+                    return AppLocalizations.of(context)!.stateUF;
                   }
                   return null;
                 },
@@ -91,15 +94,16 @@ class UpdateManagers extends StatelessWidget {
               const SizedBox(height: 20),
               TextFormField(
                 controller: _percentageController,
-                decoration: const InputDecoration(
-                  labelText: 'Porcentagem de comissão:',
+                decoration: InputDecoration(
+                  labelText:
+                      AppLocalizations.of(context)!.commissionPercentageLabel,
                   hintText: 'ex.10',
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Insira uma porcentagem';
+                    return AppLocalizations.of(context)!.insertPercentage;
                   } else if (value.contains('%')) {
-                    return 'Digite apenas números';
+                    return AppLocalizations.of(context)!.percentageOnlyNumbers;
                   }
                   return null;
                 },
@@ -130,11 +134,12 @@ class UpdateManagers extends StatelessWidget {
                               width: 300,
                               height: 220,
                               child: AlertDialog(
-                                title: const Center(
+                                title: Center(
                                   child: Text(
-                                    'Gerente Editado Com Sucesso!',
+                                    AppLocalizations.of(context)!
+                                        .successDialogTitleManager,
                                     textAlign: TextAlign.center,
-                                    style: TextStyle(
+                                    style: const TextStyle(
                                       color: Colors.black,
                                       fontWeight: FontWeight.bold,
                                       fontSize: 20.0,
@@ -167,9 +172,9 @@ class UpdateManagers extends StatelessWidget {
                         );
                       }
                     },
-                    child: const Text(
-                      'Salvar',
-                      style: TextStyle(
+                    child: Text(
+                      AppLocalizations.of(context)!.save,
+                      style: const TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
